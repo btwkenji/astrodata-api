ee974d767b50658c6ac8ce79369621898d15c92d
kenjitheman <kenjith3man@gmail.com>
2023-10-13 23:30:56 +0300

    chore: pkg naming changed

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/kenjitheman/magneconn_api/api"
-	"github.com/kenjitheman/magneconn_api/api/health"
+	"github.com/kenjitheman/astrodata-api/api"
+	"github.com/kenjitheman/astrodata-api/api/health"
 )
 
 func main() {
